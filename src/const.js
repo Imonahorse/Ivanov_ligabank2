d6f7f054c1d2d5b0ae2ba16e833df32e215ef666
Alexey <Awotjan@gmail.com>
2021-09-09 20:55:35 +0300

    поправит пробелы на сафари

--- src/const.js
+++ src/const.js
@@ -1,3 +1,5 @@
+import {instanceOf} from 'prop-types';
+
 const Purpose = {
   DEFAULT: 'default',
   MORTGAGE: 'Ипотека',
@@ -13,10 +15,10 @@ function declOfNum(number, titles) {
 const makeInputNumber = (value) => +value.replace(/[^0-9]/g, '').trim();
 
 const makeInputString = (value, string = '') => {
-  const newValue = typeof (value) === 'string' ? makeInputNumber(value) : value;
+  const newValue = typeof(value) === 'string' ? makeInputNumber(value).toString() : value.toString();
   const newString = string === '' ? string : declOfNum(newValue, string);
 
-  return (`${newValue.toLocaleString()} ${newString}`).trim();
+  return (`${newValue.replace(/(\d)(?=(\d{3})+$)/g, '$1 ').trim()} ${newString}`).trim();
 };
 
 export {Purpose, makeInputString, makeInputNumber};
